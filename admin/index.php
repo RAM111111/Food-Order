8b8ec6d32b02fddb1fa915691af6df0ed03f6ed2
RAM111111 <32421153+RAM111111@users.noreply.github.com>
2020-12-07 19:24:00 +0300

    initi pro

---
+++ admin/index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>Food Order</title>
+
+<link rel="stylesheet" href="../CSS/style.css">
+</head>
+<body>
+    <!--  menu   -->
+<div class="menu">
+<div class="wrapper">
+<ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Admin</a></li>
+    <li><a href="#">Category</a></li>
+    <li><a href="#">Food</a></li>
+    <li><a href="#">Order</a></li>
+</ul>
+</div>
+</div>
+    <!--  End-menu   -->
+
+        <!--  main-content   -->
+<div class="main-content">
+<div class="wrapper">
+main-content
+</div>
+</div>
+    <!--  End-main-content   -->
+
+            <!--  footer   -->
+<div class="footer">
+<div class="wrapper">
+<p>2020-All Rights Reserved.Ram resturent .Devlop by <a href="#">Ram</a></p>
+</div>
+</div>
+    <!--  End-footer   -->
+
+    
+</body>
+</html>
