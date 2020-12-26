f81b878edc60403f5c52dd9a29751d574b85b1a8
RAM111111 <r.r.99@windowslive.com>
2020-12-26 23:41:42 +0300

    display food

--- food/manage-food.php
+++ food/manage-food.php
@@ -19,31 +19,56 @@
     <table class="tbl-full">
       <tr>
         <th>S.N</th>
-        <th>FULL NAME</th>
-        <th>FIRST NAME</th>
-        <th>ACTION</th>
+        <th>title</th>
+        <th>price</th>
+        <th>image</th>
+        <th>featured</th>
+        <th>active</th>
+        <th>action</th>
       </tr>
-      <tr>
-        <td>1.</td>
-        <td>ram </td>
-        <td>Ram</td>
-        <td><a href="#" class="btn-secondary">update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a></td>
-      </tr>
-      <tr>
-        <td>2.</td>
-        <td>ram </td>
-        <td>Ram</td>
-        <td><a href="#" class="btn-secondary">update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a></td>
-      </tr>
-      <tr>
-        <td>3.</td>
-        <td>ram </td>
-        <td>Ram</td>
-        <td><a href="#" class="btn-secondary">update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a></td>
-      </tr>
+      <?php
+        $sql = "SELECT * FROM tbl_food";
+        $res = mysqli_query($con,$sql);
+
+        $count = mysqli_num_rows($res);
+        $i = 1;
+        if($count>0){
+          while ($row = mysqli_fetch_assoc($res)) {
+            $id = $row['id'];
+            $title = $row['title'];
+            $price = $row['price'];
+            $image_name = $row['image_name'];
+            $featured = $row['feature'];
+            $active = $row['active'];
+            ?>
+            <tr>
+              <td><?php echo $i++; ?></td>
+              <td><?php echo $title; ?></td>
+              <td><?php echo $price; ?> </td>
+              <td><?php
+                 if($image_name ==""){
+                   echo "<div class='error'>no image added</div>";
+                 }else {
+                  ?>
+                  <img src="../images/food/<?php echo $image_name; ?>" alt="" width="100px" height="75px">
+                  <?php
+                 }
+               ?></td>
+              <td><?php echo $featured; ?> </td>
+              <td><?php echo $active; ?></td>
+              <td><a href="#" class="btn-secondary">update Admin</a>
+                  <a href="#" class="btn-danger">Delete Admin</a></td>
+            </tr>
+
+            <?php
+          }
+        }else{
+          echo "<th><td colspan='7' class='error'>active</td></th>";
+        }
+
+       ?>
+
+
     </table>
   </div>
 
